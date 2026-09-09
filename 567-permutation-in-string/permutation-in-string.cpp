b46e2aa5aac3392b693d993852a87dcae24e09d3
Ayushi singh <141201272+ayushi0804@users.noreply.github.com>
2026-09-10 02:32:21 +0530

    Time: 212 ms (11.66%) | Memory: 9.9 MB (70.47%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isFreqSame(int freq1[], int freq2[]){
+        for(int i = 0; i < 26; i++){
+            if(freq1[i] != freq2[i]) return false;
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int freq[26]= {0};
+        for(int i = 0; i < s1.length(); i++){
+            freq[s1[i] - 'a']++;
+        }
+        int windsize = s1.length();
+        for(int i = 0; i < s2.length(); i++){
+            int windInd = 0, idx = i, windfreq[26] = {0};
+            while(windInd < windsize && idx < s2.length()){
+                windfreq[s2[idx] - 'a']++;
+                windInd++;
+                idx++;
+            }
+            if(isFreqSame(freq, windfreq)) return true;
+        }
+        return false;
+    }
+};
