6776a36129be4b1b4507fec09792548cf34e6b00
Ayushi singh <141201272+ayushi0804@users.noreply.github.com>
2026-09-15 21:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (56.88%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        long long xorr = 0;
+
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
+        }
+        int rightMost = xorr & (-xorr);
+
+        int b1 = 0, b2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] & rightMost)
+                b1 = b1 ^ nums[i];
+            else
+                b2 = b2 ^ nums[i];
+        }
+
+        return {b1, b2};
+    }
+};
